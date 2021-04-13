346bc5a71f43abbbbbd2ca3855c75aae8e1766cb
Jacqueline Ross <jacqueline.ross09@gmail.com>
2021-04-13 18:52:53 -0400

    service worker is working but uploading info twice

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,8 +4,6 @@ const FILES_TO_CACHE = [
     '/db.js',
     '/index.js',
     'styles.css',
-    'https://fonts.googleapis.com/css?family=Istok+Web|Montserrat:800&display=swap',
-    'https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css',
   ];
   
   const PRECACHE = 'precache-v1';
@@ -43,20 +41,19 @@ const FILES_TO_CACHE = [
   self.addEventListener('fetch', (event) => {
     if (event.request.url.startsWith(self.location.origin)) {
       event.respondWith(
-        caches.match(event.request).then((cachedResponse) => {
-          if (cachedResponse) {
-            return cachedResponse;
+        caches.open(DATA_CACHE).then((response) => {
+          if (response) {
+            return response;
           }
-  
-          return caches.open(RUNTIME).then((cache) => {
-            return fetch(event.request).then((response) => {
-              return cache.put(event.request, response.clone()).then(() => {
-                return response;
-              });
-            });
-          });
         })
       );
     }
+    return caches.match(event.request).thenn((response) => {
+        if(response){
+            return response
+        }
+        else if(event.request.headers("accept").includes("text/html")){
+            return caches.match("/")
+        }
+    })
   });
-  
